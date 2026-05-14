2841a4600c83f3a5a7ad587c7e12f4fcf3ea3b14
kadillon21 <kadillon21@gmail.com>
2026-05-14 15:23:41 -0500

    Integrate dynamic student data loading and update home screen UI to display personalized content.

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
+import 'package:flutter_app/config/app_config.dart';
 import 'package:http/http.dart' as http;
 class ApiService {
-  static const String baseUrl = 'http://localhost:8080/api'; // Replace with your API base URL
 
-  static Future<void> deleteData(String endpoint, String id) async {
-    final response = await http.delete(Uri.parse('$baseUrl/$endpoint/$id'));
-    if (response.statusCode != 200) {
-      throw Exception('Failed to delete data at $endpoint/$id');
+  static const String baseUrl = AppConfig.baseUrl; // Replace with your API base URL
+
+  static Future<Map<String, dynamic>> getStudent(int id) async {
+    final response = await http.get(Uri.parse('$baseUrl/students/$id'));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load student');
     }
   }
 }
